8c06f94130f9c9b0c515b5610fd17cf506bbcf9c
hyewon0214 <hy122903@gmail.com>
2021-05-12 13:49:02 +0900

    baseball_game.py is updated by student 1

--- baseball_game.py
+++ baseball_game.py
@@ -36,3 +36,14 @@ ax.set_xlabel('구단',fontsize=12)
 ax.set_ylabel('lank/total',fontsize=12)
 ax.legend(['lank','total'], fontsize=12)
 plt.show()
+
+from bs4 import BeautifulSoup
+import urllib.request
+response=urllib.request.urlopen('https://sports.news.naver.com/kbaseball/record/index.nhn?category=kbo&year=2020')
+soup=BeautifulSoup(response,'html.parser')
+data=[]
+for tr_tag in soup.find(id='regularTeamRecordList_table').find_all('tr'):
+    th_tag=tr_tag.find('th')
+    strong_tag=th_tag.find('strong')
+
+    span_tag=tr_tag.findAll('span')
